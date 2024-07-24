198ccbbdd7df1e3baf64a3cc3797751bcb01b642
Raymond Ruibi Muthii <raymondmuthii@gmail.com>
2024-07-24 18:04:39 +0300

    js functions

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+// saturdayFun function declaration
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // mondayWork function expression
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // wrapAdjective function
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  }
+  
